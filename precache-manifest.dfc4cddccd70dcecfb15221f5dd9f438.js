a8ab1f9a3994652cada9ca0e7ac28bab5012cfe3
root <root@DESKTOP-SAGMNUN.localdomain>
2020-01-02 11:07:18 -0500

    Updates

---
+++ precache-manifest.dfc4cddccd70dcecfb15221f5dd9f438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359160404b247c1944c9fdc11c8f6233",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "af5c1008e1af19f73e10",
+    "url": "/monsters-rolodex/static/css/main.4aa3df5e.chunk.css"
+  },
+  {
+    "revision": "fe468ebd604e1a60b50e",
+    "url": "/monsters-rolodex/static/js/2.ce0aec7e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.ce0aec7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af5c1008e1af19f73e10",
+    "url": "/monsters-rolodex/static/js/main.daf7cd0a.chunk.js"
+  },
+  {
+    "revision": "3b726521de8875249c00",
+    "url": "/monsters-rolodex/static/js/runtime-main.db19f23a.js"
+  }
+]);
